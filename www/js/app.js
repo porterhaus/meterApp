838e4e885c9b418b96702f0484668c9f76d9428d
porterhaus <bporter@porterhaus.com>
2016-01-05 15:27:48 -0600

    Added additional templates and app routing.

--- www/js/app.js
+++ www/js/app.js
@@ -19,28 +19,42 @@
     $stateProvider
 
     .state('welcome', {
-
+      url: '/welcome',
+      templateUrl: 'templates/welcome.html',
+      controller: 'WelcomeController'
     })
 
     .state('login', {
-
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginController'
     })
 
     .state('register', {
-
+      url: '/register',
+      templateUrl: 'templates/register.html',
+      controller: 'RegisterController'
     })
 
     .state('forgot', {
-
+      url: '/forgot',
+      templateUrl: 'templates/forgot.html',
+      controller: 'ForgotController'
     })
 
     .state('main', {
-
+      url: '/main',
+      templateUrl: 'templates/main.html',
+      controller: 'MainController'
     })
 
     .state('settings', {
-      
+      url: '/settings',
+      templateUrl: 'templates/settings.html',
+      controller: 'SettingsController'
     });
+
+    $urlRouterProvider.otherwise('/welcome');
 
   });
 
